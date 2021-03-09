5b021dca20ccfc44498751e798f91ceb66cd347d
Ryan Harrison <ryanharrison554@gmail.com>
2021-03-09 16:40:34 +0000

    Base all AccessServer*Errors off our BaseException

--- pyovpn_as/api/exceptions.py
+++ pyovpn_as/api/exceptions.py
@@ -3,33 +3,33 @@
 """
 import xmlrpc.client
 
-class AccessServerAuthError(BaseException):
+class AccessServerBaseException(BaseException):
+    """Used as a base exception for other errors in the API conversation
+       that are not XMLRPC Faults, e.g. bad password in SetLocalPassword
+    """
+    pass
+
+class AccessServerAuthError(AccessServerBaseException):
     """Given by Fault Code 9007, this error pertains to the user either not 
        having correct permissions or their login being incorrect.
     """
     pass
 
-class AccessServerParameterError(BaseException):
+class AccessServerParameterError(AccessServerBaseException):
     """Given by Fault Code 8002, this error indicates that the number of 
        parameters passed to the given method is incorrect
     """
     pass
 
-class AccessServerValueError(ValueError):
+class AccessServerValueError(AccessServerBaseException):
     """The Fault Code for this exception will be 9000, but the Fault String
        will begin with something like 'XMLRPCRelay: exceptions.ValueError:'
     """
     pass
 
-class AccessServerInternalError(BaseException):
+class AccessServerInternalError(AccessServerBaseException):
     """Raised when the Fault Code is 9000 and Fault String is "XMLRPC: 
        internal error"
-    """
-    pass
-
-class AccessServerBaseException(BaseException):
-    """Used as a base exception for other errors in the API conversation
-       that are not XMLRPC Faults, e.g. bad password in SetLocalPassword
     """
     pass
 
