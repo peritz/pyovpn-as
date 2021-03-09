da3a0de38a7449df428901e5e2115f77eca5b820
Ryan Harrison <ryanharrison554@gmail.com>
2021-03-09 16:21:00 +0000

    Translate XMLRPC faults into correct exception

--- pyovpn_as/api/exceptions.py
+++ pyovpn_as/api/exceptions.py
@@ -1,6 +1,8 @@
 """A module containing possible exceptions that may arise during communication
    with the XML-RPC endpoint for Access Server
 """
+import xmlrpc.client
+
 class AccessServerAuthError(BaseException):
     """Given by Fault Code 9007, this error pertains to the user either not 
        having correct permissions or their login being incorrect.
@@ -30,3 +32,45 @@ class AccessServerBaseException(BaseException):
        that are not XMLRPC Faults, e.g. bad password in SetLocalPassword
     """
     pass
+
+# -----------------
+# ---- Methods ----
+# -----------------
+def translate_error(err: Exception) -> None:
+        """Translates a given exception into one more friendly for the user
+
+        We return the exception rather than raise it because no error has
+        occurred in this method. We return so that it can be raised in the right
+        place to prevent confusion.
+
+        Args:
+            err (Exception): The Exception to translate
+
+        Returns:
+            Exception: this time it's the correct type with message and all
+        """
+        if not isinstance(err, xmlrpc.client.Fault):
+            return err
+        elif err.faultCode == 8002:
+            return AccessServerParameterError(
+                'Number of parameters is incorrect'
+            )
+        elif err.faultCode == 9007:
+            return AccessServerAuthError(
+                'Either your credentials are wrong or your permissions are not'
+                ' correct to run the given method.'
+            )
+        elif err.faultCode == 9000 and err.faultString.startswith(
+            'XMLRPCRelay: exceptions.ValueError: '
+        ):
+            start_from = len('XMLRPCRelay: exceptions.ValueError: ')
+            return AccessServerValueError(
+                f'ValueError from server: {err.faultString[start_from:]}'
+            )
+        elif err.faultCode == 9000 and \
+            err.faultString == 'XMLRPC: internal error':
+            return AccessServerInternalError(
+                'Something unknown went wrong with that call that the server '
+                'did not like'
+            )
+
