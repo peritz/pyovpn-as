8d20394c3f742ef9f754245ef99144ab247cca7f
Ryan Harrison <ryanharrison554@gmail.com>
2021-03-15 12:20:57 +0000

    Add function to create a new user (if not existing

--- pyovpn_as/_auth.py
+++ pyovpn_as/_auth.py
@@ -5,6 +5,8 @@ import logging
 from typing import Any
 
 from pyovpn_as.api.cli import AccessServerClient
+from pyovpn_as.api.exceptions import ApiClientBaseException
+
 from . import _exceptions
 
 
@@ -39,4 +41,132 @@ def get_user(
             f'"{username}" is the name of a group, not a user'
         )
     else:
+        return profile
+
+
+def create_new_user(
+    client: AccessServerClient,
+    username: str,
+    password: str=None,
+    group: str=None,
+    prop_superuser: bool=None,
+    prop_autologin: bool=None,
+    prop_deny: bool=None,
+    prop_pwd_change: bool=None,
+    prop_pwd_strength: bool=None
+) -> dict[str, Any]:
+    """Creates a user with the given parameters
+
+    This function will check if the given user exists, and if not will create it
+    via a call to UserPropPut. Then, for every additional parameter set, we will
+    make another call to UserPropPut, deleting the user if any errors occur in
+    the process.
+
+    Args:
+        client (AccessServerClient): Client used to connect to Access Server
+        username (str): Username of the user to create
+        password (str, optional): Password to set for user. If None, no
+            password will be set. Must adhere to complexity requirements.
+            Defaults to None.
+        group (str, optional): A default group to assign the user to. If set,
+            this user will inherit all options that apply to the given group.
+            Will raise an error if group does not exist. Defaults to None.
+        prop_superuser (bool, optional): Whether or not to explicitly make this
+            user an administrator. Defaults to None.
+        prop_autologin (bool, optional): Whether or not to explicitly allow this
+            user to connect without a username and password. Defaults to None.
+        prop_deny (bool, optional): Explicitly deny access to the user.
+            Defaults to None.
+        prop_pwd_change (bool, optional): Whether or not to
+            explicitly allow the user to change their password using the WebUI.
+            Defaults to None.
+        prop_pwd_strength (bool, optional): Whether or not to
+            explicitly check the complexity of this user's password when they
+            try to change it in the WebUI. Defaults to None.
+
+    Raises:
+        AccessServerProfileExistsError: username provided already exists as
+            either a user or a group
+
+    Returns:
+        dict[str, Any]: A dictionary representing the user just created
+
+    TODO Add parameter to hide profile in ui
+    """
+    # 1. Check for existence of user
+    logger.debug(
+        f'create_new_user() called with ({client}, {username}, {password},'
+        f' {group}, {prop_superuser}, {prop_autologin}, {prop_deny}, '
+        f'{prop_pwd_change}, {prop_pwd_strength})'
+    )
+    profile_dict = client.UserPropGet(pfilt=[username])
+    if profile_dict.get(username) is not None:
+        raise _exceptions.AccessServerProfileExistsError(
+            f'Profile for "{username}" already exists on the server'
+        )
+    logger.debug(f'Got user "{username}"')
+    
+    # 2. If there is a group specified, check that it exists
+    if group is not None:
+        if not isinstance(group, str):
+            raise TypeError(
+                f"Expected str for arg 'group', got {type(group)}"
+            )
+        profile_dict = client.UserPropGet(pfilt=[group,])
+        if profile_dict.get(group) is None:
+            raise _exceptions.AccessServerProfileNotFoundError(
+                f'Group "{group}" does not exist'
+            )
+        if profile_dict.get(group).get('type') != 'group':
+            raise _exceptions.AccessServerProfileExistsError(
+                f'Profile "{group}" is not a group'
+            )
+        logger.debug(f'Got group "{group}"')
+
+    # 3. Collect other parameters
+    param_dict = {}
+    params = [
+        (p, vars()[p]) for p in (
+        'prop_superuser',
+        'prop_autologin',
+        'prop_deny',
+        'prop_pwd_change',
+        'prop_pwd_strength'
+    )]
+    for p_name, p_val in params:
+        if p_val is None:
+            continue
+        elif not isinstance(p_val, bool):
+            raise TypeError(
+                f"Expected bool for arg '{p_name}', got {type(p_val)}"
+            )
+        elif p_val:
+            param_dict[p_name] = 'true'
+        else:
+            param_dict[p_name] = 'false'
+    logger.debug(f'param_dict = {param_dict}')
+    
+    # 4. Try to create the user and delete profile if any step fails
+    logger.info(f'Creating user "{username}"')
+    try:
+        for key, value in param_dict.items():
+            logger.debug(f'Setting property "{key}" on profile "{username}"')
+            client.UserPropPut(username, key, value)
+        if password is not None:
+            logger.debug(f'Setting password on profile "{username}"')
+            client.SetLocalPassword(username, password, '')
+    except ApiClientBaseException as api_err:
+        logger.error(
+            f'Could not create profile "{username}", '
+            'aborting and deleting profile...'
+        )
+        client.UserPropDelAll(username)
+        raise _exceptions.AccessServerProfileCreateError(
+            'Encountered an issue when setting properties on new user'
+        ) from api_err
+    else:
+        logger.debug(f'Fetching created profile for return...')
+        profile_dict = client.UserPropGet(pfilt=[username,])
+        profile = profile_dict.get(username)
+        assert profile is not None
         return profile
