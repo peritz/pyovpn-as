8278484eab84355385c91a39f4d93da528b7af87
Ryan Harrison <ryanharrison554@gmail.com>
2021-03-15 12:58:20 +0000

    Create wrapper which logs function calls and args

---
+++ pyovpn_as/utils.py
@@ -0,0 +1,17 @@
+"""A module that provides some useful functions and decorators
+"""
+import functools
+import logging
+
+logger = logging.getLogger(__name__)
+
+def debug_log_call(f):
+    """Logs the function called and the arguments passed at the debug level
+    """
+    functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        logger.debug(
+            f'{f.__name__}() called with *args={repr(args)}, '
+            f'**kwargs={repr(kwargs)}'
+        )
+    return wrapper
