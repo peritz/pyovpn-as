6e0a01ac8480ea31af6c80e632257133675f6aa5
Ryan Harrison <ryanharrison554@gmail.com>
2021-03-15 12:19:08 +0000

    Add base exception for high level functions

---
+++ pyovpn_as/_exceptions.py
@@ -0,0 +1,24 @@
+"""This module provides definitions for high level errors relating to the
+   functions provided by the root modules in this package.
+   
+Errors relating to high level functions in the root modules of the pyovpn_as
+package will not have a direct mapping at the API and XML-RPC level. Where the
+API may deal with issues such as password complexity, authorisation, and
+parameter validation, the exceptions defined here will deal with logical errors.
+
+An example of this may be when we attempt to create a new user, but this user
+already exists. The API will let us go ahead and overwrite the existing user's 
+properties, but this does not make sense from our perspective, and could cause
+substantial and confusing issues.
+
+Similarly, if we attempt to add a user to a group, but that group doesn't exist
+(e.g. we made a typo), we would generally want the program to tell us, as
+indeed the Web Admin UI does, but the API would not tell us, and would happily
+let us set the property without a second thought.
+"""
+
+
+class AccessServerBaseException(BaseException):
+    """Base exception for all exceptions in this module
+    """
+    pass
