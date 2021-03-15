c2d0271f1fef27c8eb7420ed2c6045893c059683
Ryan Harrison <ryanharrison554@gmail.com>
2021-03-15 11:43:14 +0000

    Test validate_client_args

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,6 +4,7 @@ import unittest
 import unittest.mock
 
 from pyovpn_as import client
+from pyovpn_as.api import exceptions
 
 
 class TestValidateEndpoint(unittest.TestCase):
@@ -62,3 +63,110 @@ class TestValidateEndpoint(unittest.TestCase):
         endpoint = 'https://ip-address:-1/'
         self.assertFalse(client.validate_endpoint(endpoint))
 
+
+@unittest.mock.patch('pyovpn_as.client.validate_endpoint', return_value=True)
+class TestValidateClientArgs(unittest.TestCase):
+    """TestCase for the validate_client_args function
+    """
+
+    def test_valid_args_returns_true(self, *args):
+        self.assertTrue(
+            client.validate_client_args(
+                'https://endpoint/',
+                'username',
+                'password'
+            )
+        )
+
+    def test_none_endpoint_raises_error(self, *args):
+        with self.assertRaisesRegex(
+            exceptions.ApiClientConfigurationError,
+            'Endpoint URL has not been set, cannot create client'
+        ):
+            client.validate_client_args(
+                None,
+                'username',
+                'password'
+            )
+
+    def test_non_str_endpoint_raises_error(self, *args):
+        with self.assertRaisesRegex(
+            exceptions.ApiClientConfigurationError,
+            'Endpoint URL is not a string, cannot create client'
+        ):
+            client.validate_client_args(
+                [],
+                'username',
+                'password'
+            )
+
+    def test_none_username_raises_error(self, *args):
+        with self.assertRaisesRegex(
+            exceptions.ApiClientConfigurationError,
+            'Username is not set, cannot create client'
+        ):
+            client.validate_client_args(
+                'https://endpoint/',
+                None,
+                'password'
+            )
+
+    def test_non_str_username_raises_error(self, *args):
+        with self.assertRaisesRegex(
+            exceptions.ApiClientConfigurationError,
+            'Username is not a string, cannot create client'
+        ):
+            client.validate_client_args(
+                'https://endpoint/',
+                [],
+                'password'
+            )
+
+    def test_colon_in_username_raises_error(self, *args):
+        with self.assertRaisesRegex(
+            exceptions.ApiClientConfigurationError,
+            'Username contains ":" character \(illegal in basic auth\), '
+            'cannot create client'
+        ):
+            client.validate_client_args(
+                'https://endpoint/',
+                'user:name',
+                'password'
+            )
+
+    def test_none_password_raises_error(self, *args):
+        with self.assertRaisesRegex(
+            exceptions.ApiClientConfigurationError,
+            'Password is not set, cannot create client'
+        ):
+            client.validate_client_args(
+                'https://endpoint/',
+                'username',
+                None
+            )
+
+    def test_non_str_password_raises_error(self, *args):
+        with self.assertRaisesRegex(
+            exceptions.ApiClientConfigurationError,
+            'Password is not a string, cannot create client'
+        ):
+            client.validate_client_args(
+                'https://endpoint/',
+                'username',
+                []
+            )
+
+    def test_colon_in_password_raises_error(self, *args):
+        with self.assertRaisesRegex(
+            exceptions.ApiClientConfigurationError,
+            'Password contains ":" character \(illegal in basic auth\), '
+            'cannot create client'
+        ):
+            client.validate_client_args(
+                'https://endpoint/',
+                'username',
+                'pass:word'
+            )
+
+if __name__ == '__main__':
+    unittest.main()
