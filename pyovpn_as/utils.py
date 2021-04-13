fbffdaa1a385dea58cb27b13d154815e6b01de25
Ryan Harrison <ryanharrison554@gmail.com>
2021-04-13 16:25:46 +0100

    Fix debug_log_call wrapper to wrap properly

--- pyovpn_as/utils.py
+++ pyovpn_as/utils.py
@@ -14,7 +14,7 @@ logger = logging.getLogger(__name__)
 def debug_log_call(f):
     """Logs the function called and the arguments passed at the debug level
     """
-    functools.wraps(f)
+    @functools.wraps(f)
     def wrapper(*args, **kwargs):
         logger.debug(
             f'{f.__name__}() called with *args={repr(args)}, '
